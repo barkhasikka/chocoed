63ee6a8805ea3d294146fc586cae575bffcfecc8
Tejal Ohara <tejalohara.93@gmail.com>
2018-09-23 14:21:07 +0530

    UI done for 3 screens

---
+++ chocoed/BehavioralViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BehavioralViewController.swift
+//  chocoed
+//
+//  Created by Tejal on 23/09/18.
+//  Copyright © 2018 barkha sikka. All rights reserved.
+//
+
+import UIKit
+
+class BehavioralViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "ic_background_pattern")
+        backgroundImage.contentMode = UIViewContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func continueAction(_ sender: Any) {
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
