324cadfcd74ab7ff3b2298d8160c0f3dc97b551a
Tejal Ohara <tejalohara.93@gmail.com>
2018-09-24 10:52:17 +0530

    common function api for getDropdownList

--- chocoed/NewEducationExperienceVC.swift
+++ chocoed/NewEducationExperienceVC.swift
@@ -18,79 +18,83 @@ class NewEducationExperienceVC: UIViewController, UIPickerViewDelegate, UIPicker
     }
     
 
-    func getDropdownList()
-    {
-        //        let params = ["access_token":"03db0f67032a1e3a82f28b476a8b81ea"] as Dictionary<String, String>
-        //        let response = MakeHttpPostRequest(url: getUserInfo, params: params)
-        //        print(response)
-        //        let vcEduProf = storyboard?.instantiateViewController(withIdentifier: "signup") as!
-        //
-        
-        let url = NSURL(string: "\(userDropDown)")
-        
-        //create the session object
-        let session = URLSession.shared
-        
-        //now create the NSMutableRequest object using the url object
-        let request = NSMutableURLRequest(url: url! as URL)
-        request.httpMethod = "POST"
+    func getDropdownList(){
         let params = ["access_token":"03db0f67032a1e3a82f28b476a8b81ea"] as Dictionary<String, String>
-        
-        do {
-            let httpBody =  try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions.prettyPrinted)
-            let httpBodyString = String(data: httpBody, encoding: String.Encoding.utf8)
-            request.httpBody = httpBodyString?.data(using: String.Encoding.utf8)
+        MakeHttpPostRequest(url: sendOtpApiURL, params: params, completion: {(success, response) -> Void in
+                print(response)
             
-        } catch let error {
-            print("error in serialization==",error.localizedDescription)
-        }
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
-            guard error == nil else {
-                print("error in the request", error ?? "")
-                DispatchQueue.main.async(execute: {
-                    let alertcontrol = UIAlertController(title: "alert!", message: error?.localizedDescription, preferredStyle: .alert)
-                    let alertaction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                    alertcontrol.addAction(alertaction)
-                    self.present(alertcontrol, animated: true, completion: nil)
-                })
-                return
-            }
-            
-            guard let data = data else {
-                print("something is wrong")
-                DispatchQueue.main.async(execute: {
-                    let alertcontrol = UIAlertController(title: "alert!", message: "not found", preferredStyle: .alert)
-                    let alertaction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                    
-                    alertcontrol.addAction(alertaction)
-                    self.present(alertcontrol, animated: true, completion: nil)
-                })
-                return
-            }
-            if let httpResponse = response as? HTTPURLResponse {
-                let statusCode = httpResponse.statusCode
-                // do whatever with the status code
-                print(statusCode)
-            }
-            
-            do {
-                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
-                    print(json)
-                    
-                    //                    let jsonobject = json[""] as? NSDictionary
-                    //                    let temp = jsonobject?.object(forKey: "userID") as? String ?? ""
-                    //
-                    //                    print(temp)
-                }
-            }catch let error {
-                print(error.localizedDescription)
-            }
-        }
-        task.resume()
+            })
+//        //        let params = ["access_token":"03db0f67032a1e3a82f28b476a8b81ea"] as Dictionary<String, String>
+//        //        let response = MakeHttpPostRequest(url: getUserInfo, params: params)
+//        //        print(response)
+//        //        let vcEduProf = storyboard?.instantiateViewController(withIdentifier: "signup") as!
+//        //
+//
+//        let url = NSURL(string: "\(userDropDown)")
+//
+//        //create the session object
+//        let session = URLSession.shared
+//
+//        //now create the NSMutableRequest object using the url object
+//        let request = NSMutableURLRequest(url: url! as URL)
+//        request.httpMethod = "POST"
+//        let params = ["access_token":"03db0f67032a1e3a82f28b476a8b81ea"] as Dictionary<String, String>
+//
+//        do {
+//            let httpBody =  try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions.prettyPrinted)
+//            let httpBodyString = String(data: httpBody, encoding: String.Encoding.utf8)
+//            request.httpBody = httpBodyString?.data(using: String.Encoding.utf8)
+//
+//        } catch let error {
+//            print("error in serialization==",error.localizedDescription)
+//        }
+//
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.addValue("application/json", forHTTPHeaderField: "Accept")
+//
+//        let task = session.dataTask(with: request as URLRequest) { (data, response, error) in
+//            guard error == nil else {
+//                print("error in the request", error ?? "")
+//                DispatchQueue.main.async(execute: {
+//                    let alertcontrol = UIAlertController(title: "alert!", message: error?.localizedDescription, preferredStyle: .alert)
+//                    let alertaction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//                    alertcontrol.addAction(alertaction)
+//                    self.present(alertcontrol, animated: true, completion: nil)
+//                })
+//                return
+//            }
+//
+//            guard let data = data else {
+//                print("something is wrong")
+//                DispatchQueue.main.async(execute: {
+//                    let alertcontrol = UIAlertController(title: "alert!", message: "not found", preferredStyle: .alert)
+//                    let alertaction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//
+//                    alertcontrol.addAction(alertaction)
+//                    self.present(alertcontrol, animated: true, completion: nil)
+//                })
+//                return
+//            }
+//            if let httpResponse = response as? HTTPURLResponse {
+//                let statusCode = httpResponse.statusCode
+//                // do whatever with the status code
+//                print(statusCode)
+//            }
+//
+//            do {
+//                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
+//                    print(json)
+//
+//                    //                    let jsonobject = json[""] as? NSDictionary
+//                    //                    let temp = jsonobject?.object(forKey: "userID") as? String ?? ""
+//                    //
+//                    //                    print(temp)
+//                }
+//            }catch let error {
+//                print(error.localizedDescription)
+//            }
+//        }
+//        task.resume()
 
     }
     
