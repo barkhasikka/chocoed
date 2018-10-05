8866f51d4a5c1e669790a4429f5be3bf07169546
Tejal Ohara <tejalohara.93@gmail.com>
2018-10-05 16:51:16 +0530

    view controller added

---
+++ chocoed/Successful complition Screens/ViewControllerMenubar.swift
@@ -0,0 +1,37 @@
+//
+//  ViewControllerMenubar2DonarViewController.swift
+//  chocoed
+//
+//  Created by Tejal on 05/10/18.
+//  Copyright © 2018 barkha sikka. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerMenubar: UIViewController {
+    var arraymenu = ["My Story","My Thought","My Progress","My Profile","Log out"]
+    let cousesImages = [UIImage(named:""),UIImage(named: ""), UIImage(named: ""), UIImage(named: ""), UIImage(named: "")]
+    
+    override func viewDidLoad() {
+     super.viewDidLoad()
+print("tejal")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
