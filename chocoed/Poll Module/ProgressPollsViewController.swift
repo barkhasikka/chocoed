707b1ad07d2687be5cd1ba9b7597375d618b0a11
Tejal Ohara <tejalohara.93@gmail.com>
2018-12-31 12:24:04 +0530

    push of option button

--- chocoed/Poll Module/ProgressPollsViewController.swift
+++ chocoed/Poll Module/ProgressPollsViewController.swift
@@ -11,6 +11,9 @@ import YLProgressBar
 
 class ProgressPollsViewController: UIViewController {
     @IBOutlet weak var progressBarYL: YLProgressBar!
+    @IBOutlet weak var progressBarYL2: YLProgressBar!
+    @IBOutlet weak var progressBarYL4: YLProgressBar!
+    @IBOutlet weak var progressBarYL3: YLProgressBar!
     var currentVotePercent = 0.0
     var maxValue = 10.0
     override func viewDidLoad() {
@@ -20,6 +23,17 @@ class ProgressPollsViewController: UIViewController {
         progressBarYL.progressTintColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
         progressBarYL.hideStripes = true
         
+        progressBarYL2.type = YLProgressBarType.flat
+        progressBarYL2.progressTintColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        progressBarYL2.hideStripes = true
+        
+        progressBarYL3.type = YLProgressBarType.flat
+        progressBarYL3.progressTintColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        progressBarYL3.hideStripes = true
+        
+        progressBarYL4.type = YLProgressBarType.flat
+        progressBarYL4.progressTintColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+        progressBarYL4.hideStripes = true
         // Do any additional setup after loading the view.
     }
 
