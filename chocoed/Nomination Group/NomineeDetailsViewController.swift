bb8714a915706d2f68d91156bca98564e4a39d02
Tejal Ohara <tejalohara.93@gmail.com>
2019-01-10 19:09:45 +0530

    back button action

--- chocoed/Nomination Group/NomineeDetailsViewController.swift
+++ chocoed/Nomination Group/NomineeDetailsViewController.swift
@@ -46,20 +46,13 @@ class NomineeDetailsViewController: UIViewController {
         
     }
 
+    @IBAction func backButtonAction(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "userNGOcell") as? UsersOfNGOViewController
+        self.present(vc!, animated: true, completion: nil)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
